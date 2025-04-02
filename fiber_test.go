5264a6b47b77e8698bf67988581232b2268e9f5a
Aldi.Rismawan <aldirvn4@gmail.com>
2025-04-02 21:23:19 +0700

    🎉 init(Add benchmark): using httptest without opening any port

---
+++ fiber_test.go
@@ -0,0 +1,28 @@
+package main_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+)
+
+func BenchmarkFiber(b *testing.B) {
+	app := fiber.New()
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "pong",
+		})
+	})
+
+	router := adaptor.FiberApp(app) //? Adaptor konversi fasthttp ke net/http agar dapat dilakukan pemanggilan ServeHTTP
+
+	req := httptest.NewRequest("GET", "/ping", http.NoBody)
+	for i := 0; i < b.N; i++ {
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, req)
+	}
+}
---
+++ gin_test.go
@@ -0,0 +1,26 @@
+package main_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func BenchmarkGin(b *testing.B) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/ping", http.NoBody)
+	for i := 0; i < b.N; i++ {
+		router.ServeHTTP(recorder, request)
+	}
+}
